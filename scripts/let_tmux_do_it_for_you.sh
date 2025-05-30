e58b1602aa6e3b308b7b00f04df579ecaff2c2e7
yongtenglei <yongtengrey@outlook.com>
2025-05-30 18:50:27 +0800

    reborn

--- scripts/let_tmux_do_it_for_you.sh
+++ scripts/let_tmux_do_it_for_you.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-SESSION="work_space"
+SESSION="ragflow_workspace"
 
-tmux has-session -t $SESSION 2>/dev/null
-
-if [$? != 0]; then
-  # Open source
-  tmux new-session -d -s $SESSION -n "open-server"
-  tmux send-keys -t $SESSION:open-server "cd ~/workspace/ragflow" C-m
-  tmux send-keys -t $SESSION:open-server "docker compose -f docker/docker-compose-base.yml up -d" C-m
-  tmux send-keys -t $SESSION:open-server "source .venv/bin/activate; \
+if ! tmux has-session -t "$SESSION" 2>/dev/null; then
+  # Open Source
+  tmux new-session -d -s "$SESSION" -n "open-server"
+  tmux send-keys -t "$SESSION:open-server" "cd ~/workspace/ragflow" C-m
+  tmux send-keys -t "$SESSION:open-server" "docker compose -f docker/docker-compose-base.yml up -d" C-m
+  tmux send-keys -t "$SESSION:open-server" "source .venv/bin/activate; \
                                           export PYTHONPATH=$(pwd); \
                                           bash docker/launch_backend_service.sh" C-m
 
-  tmux new-session -d -s $SESSION -n "open-web"
-  tmux send-keys -t $SESSION:open-server "cd ~/workspace/ragflow/web" C-m
+  tmux new-window -t "$SESSION" -n "open-web"
+  tmux send-keys -t "$SESSION:open-web" "cd ~/workspace/ragflow/web" C-m
 
   # Enterprise
-  tmux new-session -d -s $SESSION -n "close-server"
-  tmux send-keys -t $SESSION:open-server "cd ~/workspace/close/ragflow" C-m
+  tmux new-window -t "$SESSION" -n "close-server"
+  tmux send-keys -t "$SESSION:close-server" "cd ~/workspace/close/ragflow" C-m
 
-  tmux new-session -d -s $SESSION -n "close-web"
-  tmux send-keys -t $SESSION:open-server "cd ~/workspace/ragflow/web" C-m
+  tmux new-window -t "$SESSION" -n "close-web"
+  tmux send-keys -t "$SESSION:close-web" "cd ~/workspace/close/ragflow/web" C-m
 fi
 
-tmux attach-session -t $SESSION
+tmux attach-session -t "$SESSION"
